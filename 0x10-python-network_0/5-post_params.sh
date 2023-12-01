91c124dcdc61b865bdf9bcd6972a9d18c809f651
muriukialex-n <muriukialex.n@gmail.com>
2023-11-30 16:01:08 -0800

    changes

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script to post data (url-encoded) to a server
-curl -s -d "email=hr@holbertonschool.com&subject=I%20will%20always%20be%20here%20for%20PLD" -X POST "$1"
+# This script sends a POST request to a URL with specific parameters using curl
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
